2e65e22933dfd777155b3d70953c614458f03cc2
webviosystem10 <riddhee25@gmail.com>
2024-07-05 11:57:39 +0530

    heh

--- src/Components/GlobalFunction/globalFunction.js
+++ src/Components/GlobalFunction/globalFunction.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
 import { handleNavigation } from './Navigation';
-const baseName = "http://127.0.0.1:8000/api"
+const baseName = "http://192.168.2.220/UcaasS-Backend/api"
 
 
 // Creating instance of axios
 const axiosInstance = axios.create({
-    baseURL: baseName,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  baseURL: baseName,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
 const token = localStorage.getItem("token")
-if(token!==null){
-    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
+if (token !== null) {
+  axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
 }
 
-  
+
 const setAuthToken = (token) => {
-    if (token) {
-        axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    } else {
-        delete axiosInstance.defaults.headers.common['Authorization'];
-    }
+  if (token) {
+    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete axiosInstance.defaults.headers.common['Authorization'];
+  }
 };
 
 // Login function
@@ -40,86 +40,86 @@ export async function login(userName, password) {
       return res.data;
     })
     .catch(error => {
-     return error.response.data;
+      return error.response.data;
     });
 }
 
 // General Get Function
-export async function generalGetFunction (endpoint){
-    return axiosInstance.get(endpoint).then(res=>{
-        return res.data
-    }).catch(err=>{
-      if(err.response.status===401){
-        handleNavigation("/")
-        return err.response.data
-      }else{
-        return err.response.data
-      }
-      // console.log("This is error log",err.response.status);
-       
-        
-    })
+export async function generalGetFunction(endpoint) {
+  return axiosInstance.get(endpoint).then(res => {
+    return res.data
+  }).catch(err => {
+    if (err.response.status === 401) {
+      handleNavigation("/")
+      return err.response.data
+    } else {
+      return err.response.data
+    }
+    // console.log("This is error log",err.response.status);
+
+
+  })
 }
 
 // General Post function
-export async function generalPostFunction (endpoint,data){
-  return axiosInstance.post(endpoint,data).then(res=>{
-      return res.data
-  }).catch(err=>{
-    if(err.response.status===401){
+export async function generalPostFunction(endpoint, data) {
+  return axiosInstance.post(endpoint, data).then(res => {
+    return res.data
+  }).catch(err => {
+    if (err.response.status === 401) {
       handleNavigation("/")
       return err.response.data
-    }else{
+    } else {
       return err.response.data
     }
   })
 }
 
 // General Put function
-export async function generalPutFunction (endpoint,data){
-  return axiosInstance.put(endpoint,data).then(res=>{
-      return res.data
-  }).catch(err=>{
-    if(err.response.status===401){
+export async function generalPutFunction(endpoint, data) {
+  return axiosInstance.put(endpoint, data).then(res => {
+    return res.data
+  }).catch(err => {
+    if (err.response.status === 401) {
       handleNavigation("/")
       return err.response.data
-    }else{
+    } else {
       return err.response.data
     }
   })
 }
 
 // General Delete Function
-export async function generalDeleteFunction (endpoint){
-  return axiosInstance.delete(endpoint).then(res=>{
-      return res.data
-  }).catch(err=>{
-    if(err.response.status===401){
+export async function generalDeleteFunction(endpoint) {
+  return axiosInstance.delete(endpoint).then(res => {
+    return res.data
+  }).catch(err => {
+    if (err.response.status === 401) {
       handleNavigation("/")
       return err.response.data
-    }else{
+    } else {
       return err.response.data
     }
   })
 }
 
 // ImageUpload function
-export async function imageUploadFunction (endpoint,data){
-  return  axiosInstance.post(endpoint,data, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }).then(res=>{
-      return res.data
-  }).catch(err=>{
-      return err.response.data
-      
+export async function imageUploadFunction(endpoint, data) {
+  return axiosInstance.post(endpoint, data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  }).then(res => {
+    return res.data
+  }).catch(err => {
+    return err.response.data
+
   })
 }
 
 
 // Back to top function
-export const backToTop = () =>{
-    window.scrollTo(0,0);
+export const backToTop = () => {
+  window.scrollTo(0, 0);
 }
 
