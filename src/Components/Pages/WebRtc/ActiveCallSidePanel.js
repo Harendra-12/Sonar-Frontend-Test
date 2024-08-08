278059c2357f1de476b06dbc6ec104f36a3ed8d1
webviosystem11 <webviosystem11@gmail.com>
2024-08-08 17:37:16 +0530

    .

--- src/Components/Pages/WebRtc/ActiveCallSidePanel.js
+++ src/Components/Pages/WebRtc/ActiveCallSidePanel.js
@@ -1,91 +1,96 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useSessionCall} from 'react-sipjs';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useSessionCall } from "react-sipjs";
 
-function ActiveCallSidePanel({sessionId,chennel,destination}) {
-    const dispatch = useDispatch()
-    const globalSession = useSelector((state) => state.sessions);    
-    const callProgressId = useSelector((state) => state.callProgressId);
-    const {
-        isHeld,
-        session,
-        timer,
-      } = useSessionCall(sessionId);
+function ActiveCallSidePanel({ sessionId, chennel, destination }) {
+  const dispatch = useDispatch();
+  const globalSession = useSelector((state) => state.sessions);
+  const callProgressId = useSelector((state) => state.callProgressId);
+  const { isHeld, session, timer } = useSessionCall(sessionId);
 
-
-        if(session["_state"]==="Terminated"){    
-            if(callProgressId===session._id){
-                dispatch({
-                    type:"SET_CALLPROGRESSID",
-                    callProgressId:"",
-                  })
-                  dispatch({
-                    type:"SET_CALLPROGRESSDESTINATION",
-                    callProgressDestination:"",
-                  })
-                  dispatch({
-                    type:"SET_CALLPROGRESS",
-                    callProgress:false,
-                  })
-            }        
-            const updatedSession = globalSession.filter((item)=>item.id!==session._id)
-            dispatch({
-                type:"SET_SESSIONS",
-                sessions:updatedSession
-            })
-        }
-    
-      console.log("Active call",session._state);
-
-
-      
-   function handleActiveCall(id,dest){
+  if (session["_state"] === "Terminated") {
+    if (callProgressId === session._id) {
+      dispatch({
+        type: "SET_CALLPROGRESSID",
+        callProgressId: "",
+      });
+      dispatch({
+        type: "SET_CALLPROGRESSDESTINATION",
+        callProgressDestination: "",
+      });
+      dispatch({
+        type: "SET_CALLPROGRESS",
+        callProgress: false,
+      });
+    }
+    const updatedSession = globalSession.filter(
+      (item) => item.id !== session._id
+    );
     dispatch({
-        type:"SET_CALLPROGRESSID",
-        callProgressId:id,
-      })
-      dispatch({
-        type:"SET_CALLPROGRESSDESTINATION",
-        callProgressDestination:dest,
-      })
-      dispatch({
-        type:"SET_CALLPROGRESS",
-        callProgress:true,
-      })
-   }
-    
-    return (
-       <>
-       {isHeld?<div onClick={()=>handleActiveCall(session._id,destination)} className='col-12 callItem hold'>
-                        <div className='profilepicHolder'>
-                            {chennel+1}
-                        </div>
-                        <div className='callContent'>
-                            <h4>Line {chennel+1}</h4>
-                            <h5>{destination}</h5>
-                        </div>
-                    </div>:session._state==="Initial"?<div onClick={()=>handleActiveCall(session._id,destination)}  className='col-12 callItem ringing'>
-                        <div className='profilepicHolder'>
-                            {chennel+1}
-                        </div>
-                        <div className='callContent'>
-                            <h4>Line {chennel+1}</h4>
-                            <h5>{destination}</h5>
-                        </div>
-                    </div>:<div onClick={()=>handleActiveCall(session._id,destination)}  className='col-12 callItem active'>
-                        <div className='profilepicHolder'>
-                            {chennel+1}
-                        </div>
-                        <div className='callContent'>
-                            <h4>Line {chennel+1}</h4>
-                            <h5>{destination}
-                                {/* <span className='float-end'>02:23
+      type: "SET_SESSIONS",
+      sessions: updatedSession,
+    });
+  }
+
+  console.log("Active call", session._state);
+
+  function handleActiveCall(id, dest) {
+    dispatch({
+      type: "SET_CALLPROGRESSID",
+      callProgressId: id,
+    });
+    dispatch({
+      type: "SET_CALLPROGRESSDESTINATION",
+      callProgressDestination: dest,
+    });
+    dispatch({
+      type: "SET_CALLPROGRESS",
+      callProgress: true,
+    });
+  }
+
+  return (
+    <>
+      {isHeld ? (
+        <div
+          onClick={() => handleActiveCall(session._id, destination)}
+          className="col-12 callItem hold"
+        >
+          <div className="profilepicHolder">{chennel + 1}</div>
+          <div className="callContent">
+            <h4>Line {chennel + 1}</h4>
+            <h5>{destination}</h5>
+          </div>
+        </div>
+      ) : session._state === "Initial" ? (
+        <div
+          onClick={() => handleActiveCall(session._id, destination)}
+          className="col-12 callItem ringing"
+        >
+          <div className="profilepicHolder">{chennel + 1}</div>
+          <div className="callContent">
+            <h4>Line {chennel + 1}</h4>
+            <h5>{destination}</h5>
+          </div>
+        </div>
+      ) : (
+        <div
+          onClick={() => handleActiveCall(session._id, destination)}
+          className="col-12 callItem active"
+        >
+          <div className="profilepicHolder">{chennel + 1}</div>
+          <div className="callContent">
+            <h4>Line {chennel + 1}</h4>
+            <h5>
+              {destination}
+              {/* <span className='float-end'>02:23
                                     </span> */}
-                                    </h5>
-                        </div>
-                    </div>}
-                    
-                    {/* <div className='col-12 callItem active'>
+            </h5>
+          </div>
+        </div>
+      )}
+
+      {/* <div className='col-12 callItem active'>
                         <div className='profilepicHolder'>
                             2
                         </div>
@@ -112,8 +117,8 @@ function ActiveCallSidePanel({sessionId,chennel,destination}) {
                             <h5>1003 <span className='float-end'>02:23</span></h5>
                         </div>
                     </div> */}
-              </>
-    )
+    </>
+  );
 }
 
-export default ActiveCallSidePanel
+export default ActiveCallSidePanel;
