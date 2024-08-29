432ba0d5b4a12e3cd61a3bb7dd18d850bda8a6d8
webviosystem11 <webviosystem11@gmail.com>
2024-08-29 12:20:34 +0530

    DTMF message done

--- src/Components/Pages/WebRtc/OngoingCall.js
+++ src/Components/Pages/WebRtc/OngoingCall.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useSessionCall } from "react-sipjs";
 import { CallTimer } from "./CallTimer";
@@ -6,6 +6,8 @@ import { SessionState } from "sip.js";
 
 function OngoingCall({ setHangupRefresh, hangupRefresh }) {
   const callProgressId = useSelector((state) => state.callProgressId);
+  const [hideDialpad,setHideDialpad] = useState(false)
+  const [destNumber,setDestNumber]=useState("")
   const callProgressDestination = useSelector(
     (state) => state.callProgressDestination
   );
@@ -24,6 +26,19 @@ function OngoingCall({ setHangupRefresh, hangupRefresh }) {
     direction,
     timer,
   } = useSessionCall(callProgressId);
+
+  const handleDigitPress = (digit) => {
+    if (session) {
+      const dtmfSender = session.sessionDescriptionHandler.peerConnection.getSenders().find(sender => sender.dtmf);
+      if (dtmfSender && dtmfSender.dtmf) {
+        dtmfSender.dtmf.insertDTMF(digit);
+      } else {
+        console.error('DTMF sender not available');
+      }
+    } else {
+      console.error('No active session found');
+    }
+  };
   return (
     <>
       <div className="caller">
@@ -75,7 +90,11 @@ function OngoingCall({ setHangupRefresh, hangupRefresh }) {
             >
               <i className="fa-thin fa-microphone-slash" />
             </button>
-            <button className="appPanelButtonCaller" effect="ripple">
+            <button
+            onClick={() => setHideDialpad(!hideDialpad)}
+            className={
+              hideDialpad ? "appPanelButtonCaller active" : "appPanelButtonCaller"
+            } effect="ripple">
               <i className="fa-thin fa-grid" />
             </button>
             <button className="appPanelButtonCaller" effect="ripple">
@@ -107,8 +126,141 @@ function OngoingCall({ setHangupRefresh, hangupRefresh }) {
               <i className="fa-thin fa-phone-hangup" />
             </button>
           </div>
+          
+        </div>
+        {hideDialpad?
+        <div id="dialPad" className="inCall">
+        <div className="container h-100">
+          <div className="row align-items-center justify-content-center h-100">
+            <div className="col-xl-5 col-md-6 col-11 dialPadContainer p-2">
+              <div className="d-flex justify-content-end pt-1 pb-1 px-2">
+                {/* <div>
+                  <i className="fa-light fa-address-book fs-5" />
+                </div> */}
+                {/* <div>
+                  <h3>Dial Number:</h3>
+                </div> */}
+                <div
+                  onClick={() => setHideDialpad(false)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <i className="fa-regular fa-xmark fs-5 text-white" />
+                </div>
+              </div>
+              <div className="mb-2">
+                {/* <span>Outbound ID: (999) 999-9999</span> */}
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder=""
+                  className="dialerInput"
+                  disabled={true}
+                  value={destNumber}
+                  // onChange={(e) => setDestNumber(e.target.value)}
+                  // onChange={handleInputChange}
+                />
+              </div>
+              <div className="dialerWrap mt-2">
+                <div
+                  className="col-4"
+                  onClick={() => {setDestNumber(destNumber + "1");handleDigitPress(1)}}
+                >
+                  <h4>1</h4>
+                  <h6>&nbsp;</h6>
+                </div>
+                <div
+                  className="col-4"
+                  onClick={() => {setDestNumber(destNumber + "2");handleDigitPress(2)}}
+                >
+                  <h4>2</h4>
+                  <h6>ABC</h6>
+                </div>
+                <div
+                  className="col-4"
+                  onClick={() => {setDestNumber(destNumber + "3");handleDigitPress(3)}}
+                >
+                  <h4>3</h4>
+                  <h6>DEF</h6>
+                </div>
+                <div
+                  className="col-4"
+                  onClick={() => {setDestNumber(destNumber + "4");handleDigitPress(4)}}
+                >
+                  <h4>4</h4>
+                  <h6>GHI</h6>
+                </div>
+                <div
+                  className="col-4"
+                  onClick={() => {setDestNumber(destNumber + "5");handleDigitPress(5)}}
+                >
+                  <h4>5</h4>
+                  <h6>JKL</h6>
+                </div>
+                <div
+                  className="col-4"
+                  onClick={() => {setDestNumber(destNumber + "6");handleDigitPress(6)}}
+                >
+                  <h4>6</h4>
+                  <h6>MNO</h6>
+                </div>
+                <div
+                  className="col-4"
+                  onClick={() => {setDestNumber(destNumber + "7");handleDigitPress(7)}}
+                >
+                  <h4>7</h4>
+                  <h6>PQRS</h6>
+                </div>
+                <div
+                  className="col-4"
+                  onClick={() =>{ setDestNumber(destNumber + "8");handleDigitPress(8)}}
+                >
+                  <h4>8</h4>
+                  <h6>TUV</h6>
+                </div>
+                <div
+                  className="col-4"
+                  onClick={() => {setDestNumber(destNumber + "9");handleDigitPress(9)}}
+                >
+                  <h4>9</h4>
+                  <h6>WXYZ</h6>
+                </div>
+                <div className="col-4"
+                onClick={() => {setDestNumber(destNumber + "*");handleDigitPress("*")}}
+                >
+                  <h4>
+                    <i className="fa-light fa-asterisk" />
+                  </h4>
+                </div>
+                <div
+                  className="col-4"
+                  onClick={() => {setDestNumber(destNumber + "0");handleDigitPress(0)}}
+                >
+                  <h4>0</h4>
+                  <h6>+</h6>
+                </div>
+                <div className="col-4"
+                onClick={() => {setDestNumber(destNumber + "#");handleDigitPress("#")}}
+                >
+                  <h4>
+                    <i className="fa-light fa-hashtag" />
+                  </h4>
+                </div>
+              </div>
+              {/* <div 
+                // onClick={onSubmit}
+                >
+                <button className="callButton">
+                  <i className="fa-thin fa-phone" />
+                </button>
+              </div> */}
+            </div>
+          </div>
         </div>
       </div>
+      :""}
+      </div>
+
     </>
   );
 }
