02b2c344f9ff57333791fa86010e855d1f98959a
webviosystem10 <riddhee25@gmail.com>
2024-11-04 15:59:27 +0530

    CallCenterQueue UI Update

--- src/Components/Pages/CallCenter/CallCenterSettings.js
+++ src/Components/Pages/CallCenter/CallCenterSettings.js
@@ -90,7 +90,7 @@ const CallCenterSettings = () => {
       <section id="phonePage">
         <div className="container-fluid px-0">
           <Header title="Call Center Settings" />
-          <div id="subPageHeader">
+          {/* <div id="subPageHeader">
             <div className="col-xl-9 my-auto"></div>
             <div className="col-xl-3 ps-2">
               <div className="d-flex justify-content-end">
@@ -108,13 +108,45 @@ const CallCenterSettings = () => {
                 </button>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
         <div className="col-xl-12">
           {loading && <CircularLoader />}
-          <div className="mx-2" id="detailsContent">
-            <form>
-              {/* <div className="formRow col-xl-3">
+          <div className="overviewTableWrapper">
+            <div className="overviewTableChild">
+              <div className="d-flex flex-wrap">
+                <div className="col-12">
+                  <div className="heading">
+                    <div className="content">
+                      <h4>Call Center Settings</h4>
+                    </div>
+                    <div className="buttonGroup">
+                      <button
+                        onClick={() => {
+                          navigate(-1);
+                        }}
+                        type="button"
+                        effect="ripple"
+                        className="panelButton gray"
+                      >
+                        <span className="text">Back</span>
+                        <span className="icon"><i class="fa-solid fa-caret-left"></i></span>
+                      </button>
+                      <button
+                        effect="ripple"
+                        className="panelButton"
+                        onClick={handleFormSubmit}
+                      >
+                        <span className="text">Save</span>
+                        <span className="icon"><i class="fa-solid fa-floppy-disk"></i></span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-12" style={{ padding: '25px 23px', borderBottom: '1px solid #ddd' }}>
+                <form>
+                  {/* <div className="formRow col-xl-3">
                 <div className="formLabel">
                   <label className="text-dark">Greeting</label>
                   <label className="formItemDesc">
@@ -132,105 +164,107 @@ const CallCenterSettings = () => {
                   </select>
                 </div>
               </div> */}
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Strategy</label>
-                  <label className="formItemDesc">
-                    Select the queue ring strategy.
-                  </label>
-                </div>
-                <div className="col-6">
-                  <select
-                    {...register("strategy")}
-                    className="formItem me-0"
-                    style={{ width: "100%" }}
-                  >
-                    <option value="ring-all">Ring All</option>
-                    <option value="longest-idle-agent">
-                      Longest Idle Agent
-                    </option>
-                    <option value="round-robin">Round Robin</option>
-                    <option value="top-down">Top Down</option>
-                    <option value="agent-with-least-talk-time">
-                      Agent with least talk time
-                    </option>
-                    <option value="agent-with-fewest-calls">
-                      Agent with fewest calls
-                    </option>
-                    <option value="sequentially-by-aget-order">
-                      Sequentially by agent order
-                    </option>
-                    <option value="ring-progressively">
-                      Ring Progressively
-                    </option>
-                    <option value="random">Random</option>
-                  </select>
-                </div>
+                  <div className="formRow col-xl-3">
+                    <div className="formLabel">
+                      <label className="text-dark">Strategy</label>
+                      <label className="formItemDesc">
+                        Select the queue ring strategy.
+                      </label>
+                    </div>
+                    <div className="col-6">
+                      <select
+                        {...register("strategy")}
+                        className="formItem me-0"
+                        style={{ width: "100%" }}
+                      >
+                        <option value="ring-all">Ring All</option>
+                        <option value="longest-idle-agent">
+                          Longest Idle Agent
+                        </option>
+                        <option value="round-robin">Round Robin</option>
+                        <option value="top-down">Top Down</option>
+                        <option value="agent-with-least-talk-time">
+                          Agent with least talk time
+                        </option>
+                        <option value="agent-with-fewest-calls">
+                          Agent with fewest calls
+                        </option>
+                        <option value="sequentially-by-aget-order">
+                          Sequentially by agent order
+                        </option>
+                        <option value="ring-progressively">
+                          Ring Progressively
+                        </option>
+                        <option value="random">Random</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="formRow col-xl-3">
+                    <div className="formLabel">
+                      <label className="text-dark">Record</label>
+                      <label className="formItemDesc">Save the recording.</label>
+                    </div>
+                    <div className="col-6">
+                      <select
+                        {...register("recording_enabled")}
+                        className="formItem me-0"
+                        style={{ width: "100%" }}
+                      >
+                        <option value={true}>True</option>
+                        <option value={false}>False</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="formRow col-xl-3">
+                    <div className="formLabel">
+                      <label className="text-dark">Time Base Score</label>
+                    </div>
+                    <div className="col-6">
+                      <select
+                        {...register("time_base_score")}
+                        className="formItem me-0"
+                        style={{ width: "100%" }}
+                      >
+                        <option value="queue">Queue</option>
+                        <option value="system">System</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="formRow col-xl-3">
+                    <div className="formLabel">
+                      <label className="text-dark">Tier Rules Apply</label>
+                    </div>
+                    <div className="col-6">
+                      <select
+                        {...register("tier_rules_apply")}
+                        className="formItem me-0"
+                        style={{ width: "100%" }}
+                      >
+                        <option value={1}>True</option>
+                        <option value={0}>False</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="formRow col-xl-3">
+                    <div className="formLabel">
+                      <label className="text-dark">
+                        Tier Rule Wait Multiply Level
+                      </label>
+                    </div>
+                    <div className="col-6">
+                      <select
+                        {...register("tier_rule_wait_multiply_level")}
+                        className="formItem me-0"
+                        style={{ width: "100%" }}
+                      >
+                        <option value={1}>True</option>
+                        <option value={0}>False</option>
+                      </select>
+                    </div>
+                  </div>
+                </form>
               </div>
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Record</label>
-                  <label className="formItemDesc">Save the recording.</label>
-                </div>
-                <div className="col-6">
-                  <select
-                    {...register("recording_enabled")}
-                    className="formItem me-0"
-                    style={{ width: "100%" }}
-                  >
-                    <option value={true}>True</option>
-                    <option value={false}>False</option>
-                  </select>
-                </div>
-              </div>
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Time Base Score</label>
-                </div>
-                <div className="col-6">
-                  <select
-                    {...register("time_base_score")}
-                    className="formItem me-0"
-                    style={{ width: "100%" }}
-                  >
-                    <option value="queue">Queue</option>
-                    <option value="system">System</option>
-                  </select>
-                </div>
-              </div>
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Tier Rules Apply</label>
-                </div>
-                <div className="col-6">
-                  <select
-                    {...register("tier_rules_apply")}
-                    className="formItem me-0"
-                    style={{ width: "100%" }}
-                  >
-                    <option value={1}>True</option>
-                    <option value={0}>False</option>
-                  </select>
-                </div>
-              </div>
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">
-                    Tier Rule Wait Multiply Level
-                  </label>
-                </div>
-                <div className="col-6">
-                  <select
-                    {...register("tier_rule_wait_multiply_level")}
-                    className="formItem me-0"
-                    style={{ width: "100%" }}
-                  >
-                    <option value={1}>True</option>
-                    <option value={0}>False</option>
-                  </select>
-                </div>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
       </section>
