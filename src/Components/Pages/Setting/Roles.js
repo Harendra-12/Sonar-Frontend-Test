59941720dde018b34d879c6f043d102fb0a4295a
webviosystem11 <webviosystem11@gmail.com>
2024-11-18 12:51:25 +0530

    role change

--- src/Components/Pages/Setting/Roles.js
+++ src/Components/Pages/Setting/Roles.js
@@ -438,58 +438,66 @@ function Roles() {
                                 {selectedRole}
                               </span>
                             </div>
-                            <div className="col-2 text-end">
-                              <button
-                                className="btn btn-success py-1 px-2"
-                                onClick={handlePermissionSave}
-                              >
-                                <i class="fa-duotone fa-check-double me-2"></i>{" "}
-                                Save
-                              </button>
-                            </div>
+                            {selectedRole !== "Agent" &&
+                              <div className="col-2 text-end">
+                                <button
+                                  className="btn btn-success py-1 px-2"
+                                  onClick={handlePermissionSave}
+                                >
+                                  <i class="fa-duotone fa-check-double me-2"></i>{" "}
+                                  Save
+                                </button>
+                              </div>
+                            }
                           </div>
                         </div>
-                        {filteredPermission &&
-                          Object.keys(filteredPermission).map((item, key) => (
-                            <div className="permissionListWrapper" key={key}>
-                              <div className="header d-flex align-items-center">
-                                <div className="col-5">
-                                  <input
-                                    type="checkbox"
-                                    checked={parentChecked[item]}
-                                    onChange={() =>
-                                      handleParentCheckboxChange(item)
-                                    }
-                                  />
-                                  <label className="ms-2">{item}</label>
-                                </div>
-                              </div>
-                              <div className="row px-2 pt-1 border-bottom">
-                                {filteredPermission[item].map(
-                                  (innerItem, key) => (
-                                    <div
-                                      className="col-xl-2 col-md-4 col-6"
-                                      key={key}
-                                    >
+                        {selectedRole === "Agent" ?
+                          <div className="col-xl-12">This is will apper when role is Agent</div>
+                          :
+                          <>
+                            {filteredPermission &&
+                              Object.keys(filteredPermission).map((item, key) => (
+                                <div className="permissionListWrapper" key={key}>
+                                  <div className="header d-flex align-items-center">
+                                    <div className="col-5">
                                       <input
                                         type="checkbox"
-                                        id={`permission-${innerItem.id}`}
-                                        checked={selectedPermission.includes(
-                                          innerItem.id
-                                        )}
+                                        checked={parentChecked[item]}
                                         onChange={() =>
-                                          handleCheckboxChange(innerItem.id)
+                                          handleParentCheckboxChange(item)
                                         }
                                       />
-                                      <label className="formLabel text-capitalize">
-                                        {innerItem.action}
-                                      </label>
+                                      <label className="ms-2">{item}</label>
                                     </div>
-                                  )
-                                )}
-                              </div>
-                            </div>
-                          ))}
+                                  </div>
+                                  <div className="row px-2 pt-1 border-bottom">
+                                    {filteredPermission[item].map(
+                                      (innerItem, key) => (
+                                        <div
+                                          className="col-xl-2 col-md-4 col-6"
+                                          key={key}
+                                        >
+                                          <input
+                                            type="checkbox"
+                                            id={`permission-${innerItem.id}`}
+                                            checked={selectedPermission.includes(
+                                              innerItem.id
+                                            )}
+                                            onChange={() =>
+                                              handleCheckboxChange(innerItem.id)
+                                            }
+                                          />
+                                          <label className="formLabel text-capitalize">
+                                            {innerItem.action}
+                                          </label>
+                                        </div>
+                                      )
+                                    )}
+                                  </div>
+                                </div>
+                              ))}
+                          </>
+                        }
                       </div>
                     </div>
                   </div>
