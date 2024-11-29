e53a2e9545af1580e6619458e6597a30439f7003
webviosystem10 <riddhee25@gmail.com>
2024-11-29 13:31:21 +0530

    1

--- src/Components/Pages/DeviceProvisioning/DeviceProvisioningAdd.js
+++ src/Components/Pages/DeviceProvisioning/DeviceProvisioningAdd.js
@@ -69,7 +69,7 @@ const DeviceProvisioningAdd = () => {
       <section id="phonePage">
         <div className="container-fluid px-0">
           <Header title="Device Provisioning Add" />
-          <div id="subPageHeader">
+          {/* <div id="subPageHeader">
             <div className="col-xl-9 my-auto">
               <p className="mb-0">Add an Device Provisioning</p>
             </div>
@@ -77,7 +77,7 @@ const DeviceProvisioningAdd = () => {
               <div className="d-flex justify-content-end">
                 <button
                   effect="ripple"
-                  className="panelButton"
+                  className="panelButton gray"
                   onClick={() => {
                     navigate(-1);
                     backToTop();
@@ -100,7 +100,7 @@ const DeviceProvisioningAdd = () => {
                 </button>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
 
         <div className="col-xl-12" style={{ overflow: "auto" }}>
@@ -111,113 +111,163 @@ const DeviceProvisioningAdd = () => {
           ) : (
             ""
           )}
-          <div className="mx-2" id="detailsContent">
-            <form>
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Address</label>
-                  <label htmlFor="data" className="formItemDesc">
-                    Select a address
-                  </label>
+          <div className="overviewTableWrapper">
+            <div className="overviewTableChild">
+              <div className="d-flex flex-wrap">
+                <div className="col-12">
+                  <div className="heading">
+                    <div className="content">
+                      <h4>Device Provisioning Add</h4>
+                      <p>
+                        Add an Device Provisioning
+                      </p>
+                    </div>
+                    <div className="buttonGroup">
+                      <button
+                        onClick={() => {
+                          navigate(-1);
+                          backToTop();
+                        }}
+                        type="button"
+                        effect="ripple"
+                        className="panelButton gray"
+                      >
+                        <span className="text">Back</span>
+                        <span className="icon">
+                          <i class="fa-solid fa-caret-left"></i>
+                        </span>
+                      </button>
+                      <button
+                        effect="ripple"
+                        className="panelButton"
+                        onClick={handleFormSubmit}
+                      >
+                        <span className="text">Save</span>
+                        <span className="icon">
+                          <i class="fa-solid fa-floppy-disk"></i>
+                        </span>
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-6">
-                  <input
-                    value={extensionData.extension}
-                    disabled
-                    type="text"
-                    name="address"
-                    className="formItem"
-                  />
-                  {errors.address && (
-                    <ErrorMessage text={errors.address.message} />
-                  )}
-                </div>
-              </div>
+                <div
+                  className="col-12"
+                  style={{
+                    padding: "25px 23px",
+                    borderBottom: "1px solid #ddd",
+                  }}
+                >
+                  <form>
+                    <div className="formRow col-xl-3">
+                      <div className="formLabel">
+                        <label className="text-dark">Address</label>
+                        <label htmlFor="data" className="formItemDesc">
+                          Select a address
+                        </label>
+                      </div>
+                      <div className="col-6">
+                        <input
+                          value={extensionData.extension}
+                          disabled
+                          type="text"
+                          name="address"
+                          className="formItem"
+                        />
+                        {errors.address && (
+                          <ErrorMessage text={errors.address.message} />
+                        )}
+                      </div>
+                    </div>
 
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Transport</label>
-                  <label htmlFor="data" className="formItemDesc">
-                    Select a transport
-                  </label>
-                </div>
-                <div className="col-6">
-                  <select
-                    value={watch().transport}
-                    className="formItem"
-                    name="transport "
-                    id="selectFormRow"
-                    {...register("transport", {
-                      ...requiredValidator,
-                    })}
-                  >
-                    <option disabled value="">
-                      Chose a vendor
-                    </option>
-                    <option selected value="TCPpreferred">
-                      TCP Preferred
-                    </option>
-                    <option value="UDPOnly">UDP Only</option>
-                    <option value="TLS">TLS</option>
-                    <option value="TCPOnly">TCP Only</option>
-                  </select>
-                  {errors.transport && (
-                    <ErrorMessage text={errors.transport.message} />
-                  )}
-                </div>
-              </div>
+                    <div className="formRow col-xl-3">
+                      <div className="formLabel">
+                        <label className="text-dark">Transport</label>
+                        <label htmlFor="data" className="formItemDesc">
+                          Select a transport
+                        </label>
+                      </div>
+                      <div className="col-6">
+                        <select
+                          value={watch().transport}
+                          className="formItem"
+                          name="transport "
+                          id="selectFormRow"
+                          {...register("transport", {
+                            ...requiredValidator,
+                          })}
+                        >
+                          <option disabled value="">
+                            Chose a vendor
+                          </option>
+                          <option selected value="TCPpreferred">
+                            TCP Preferred
+                          </option>
+                          <option value="UDPOnly">UDP Only</option>
+                          <option value="TLS">TLS</option>
+                          <option value="TCPOnly">TCP Only</option>
+                        </select>
+                        {errors.transport && (
+                          <ErrorMessage text={errors.transport.message} />
+                        )}
+                      </div>
+                    </div>
 
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Port</label>
-                  <label htmlFor="data" className="formItemDesc">
-                    Select a port
-                  </label>
-                </div>
-                <div className="col-6">
-                  <input
-                    type="text"
-                    name="address"
-                    className="formItem"
-                    minLength="3"
-                    maxLength="4"
-                    placeholder="5070"
-                    {...register("port", {
-                      ...requiredValidator,
-                      ...numberValidator,
-                      ...lengthValidator(3, 4),
-                    })}
-                    onKeyDown={restrictToNumbers}
-                  />
-                  {errors.port && <ErrorMessage text={errors.port.message} />}
-                </div>
-              </div>
+                    <div className="formRow col-xl-3">
+                      <div className="formLabel">
+                        <label className="text-dark">Port</label>
+                        <label htmlFor="data" className="formItemDesc">
+                          Select a port
+                        </label>
+                      </div>
+                      <div className="col-6">
+                        <input
+                          type="text"
+                          name="address"
+                          className="formItem"
+                          minLength="3"
+                          maxLength="4"
+                          placeholder="5070"
+                          {...register("port", {
+                            ...requiredValidator,
+                            ...numberValidator,
+                            ...lengthValidator(3, 4),
+                          })}
+                          onKeyDown={restrictToNumbers}
+                        />
+                        {errors.port && <ErrorMessage text={errors.port.message} />}
+                      </div>
+                    </div>
 
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Serial Number</label>
-                  <label htmlFor="data" className="formItemDesc">
-                    Enter serial number
-                  </label>
-                </div>
-                <div className="col-6">
-                  <input
-                    type="text"
-                    name="serial_number"
-                    className="formItem"
-                    placeholder="124abc"
-                    {...register("serial_number", {
-                      ...requiredValidator,
-                    })}
-                    onKeyDown={restrictToMacAddress}
-                  />
-                  {errors.serial_number && (
-                    <ErrorMessage text={errors.serial_number.message} />
-                  )}
+                    <div className="formRow col-xl-3">
+                      <div className="formLabel">
+                        <label className="text-dark">Serial Number</label>
+                        <label htmlFor="data" className="formItemDesc">
+                          Enter serial number
+                        </label>
+                      </div>
+                      <div className="col-6">
+                        <input
+                          type="text"
+                          name="serial_number"
+                          className="formItem"
+                          placeholder="124abc"
+                          {...register("serial_number", {
+                            ...requiredValidator,
+                          })}
+                          onKeyDown={restrictToMacAddress}
+                        />
+                        {errors.serial_number && (
+                          <ErrorMessage text={errors.serial_number.message} />
+                        )}
+                      </div>
+                    </div>
+                  </form>
                 </div>
               </div>
-            </form>
+            </div>
           </div>
+
+
         </div>
       </section>
     </main>
--- src/Components/Pages/DeviceProvisioning/DeviceProvisioningEdit.js
+++ src/Components/Pages/DeviceProvisioning/DeviceProvisioningEdit.js
@@ -102,7 +102,7 @@ const DeviceProvisioningEdit = () => {
       <section id="phonePage">
         <div className="container-fluid px-0">
           <Header title="Device Provisioning Edit" />
-          <div id="subPageHeader">
+          {/* <div id="subPageHeader">
             <div className="col-xl-9 my-auto">
               <p className="mb-0">Edit Device Provisioning</p>
             </div>
@@ -110,7 +110,7 @@ const DeviceProvisioningEdit = () => {
               <div className="d-flex justify-content-end">
                 <button
                   effect="ripple"
-                  className="panelButton"
+                  className="panelButton gray"
                   onClick={() => {
                     navigate(-1);
                     backToTop();
@@ -146,7 +146,7 @@ const DeviceProvisioningEdit = () => {
                 </button>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
 
         <div className="col-xl-12" style={{ overflow: "auto" }}>
@@ -157,113 +157,178 @@ const DeviceProvisioningEdit = () => {
           ) : (
             ""
           )}
-          <div className="mx-2" id="detailsContent">
-            <form>
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Address</label>
-                  <label htmlFor="data" className="formItemDesc">
-                    Select a address
-                  </label>
+          <div className="overviewTableWrapper">
+            <div className="overviewTableChild">
+              <div className="d-flex flex-wrap">
+                <div className="col-12">
+                  <div className="heading">
+                    <div className="content">
+                      <h4>Device Provisioning Edit</h4>
+                      <p>
+                        Edit Device Provisioning
+                      </p>
+                    </div>
+                    <div className="buttonGroup">
+                      <button
+                        onClick={() => {
+                          navigate(-1);
+                          backToTop();
+                        }}
+                        type="button"
+                        effect="ripple"
+                        className="panelButton gray"
+                      >
+                        <span className="text">Back</span>
+                        <span className="icon">
+                          <i class="fa-solid fa-caret-left"></i>
+                        </span>
+                      </button>
+                      <button
+                        effect="ripple"
+                        className="panelButton delete"
+                        onClick={() => {
+                          setPopUp(true);
+                          setDeleteId(provisionings.id);
+                        }}
+                      >
+                        <span className="text">Delete</span>
+                        <span className="icon">
+                          <i class="fa-solid fa-trash"></i>
+                        </span>
+                      </button>
+                      <button
+                        effect="ripple"
+                        className="panelButton"
+                        onClick={() => {
+                          setPopUp(true);
+                          setDeleteId(provisionings.id);
+                        }}
+                      >
+                        <span className="text">Update</span>
+                        <span className="icon">
+                          <i class="fa-solid fa-floppy-disk"></i>
+                        </span>
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-6">
-                  <input
-                    type="text"
-                    className="formItem"
-                    value={provisionings.address}
-                    disabled
-                  />
+                <div
+                  className="col-12"
+                  style={{
+                    padding: "25px 23px",
+                    borderBottom: "1px solid #ddd",
+                  }}
+                >
+                  <form>
+                    <div className="formRow col-xl-3">
+                      <div className="formLabel">
+                        <label className="text-dark">Address</label>
+                        <label htmlFor="data" className="formItemDesc">
+                          Select a address
+                        </label>
+                      </div>
+                      <div className="col-6">
+                        <input
+                          type="text"
+                          className="formItem"
+                          value={provisionings.address}
+                          disabled
+                        />
 
-                  {errors.address && (
-                    <ErrorMessage text={errors.address.message} />
-                  )}
+                        {errors.address && (
+                          <ErrorMessage text={errors.address.message} />
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="formRow col-xl-3">
+                      <div className="formLabel">
+                        <label className="text-dark">Transport</label>
+                        <label htmlFor="data" className="formItemDesc">
+                          Select a transport
+                        </label>
+                      </div>
+                      <div className="col-6">
+                        <select
+                          value={watch().transport}
+                          className="formItem"
+                          name="transport "
+                          id="selectFormRow"
+                          {...register("transport", {
+                            ...requiredValidator,
+                          })}
+                        >
+                          <option disabled value="">
+                            Chose a vendor
+                          </option>
+                          <option selected value="TCPpreferred">
+                            TCP Preferred
+                          </option>
+                          <option value="UDPOnly">UDP Only</option>
+                          <option value="TLS">TLS</option>
+                          <option value="TCPOnly">TCP Only</option>
+                        </select>
+                        {errors.transport && (
+                          <ErrorMessage text={errors.transport.message} />
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="formRow col-xl-3">
+                      <div className="formLabel">
+                        <label className="text-dark">Port</label>
+                        <label htmlFor="data" className="formItemDesc">
+                          Select a port
+                        </label>
+                      </div>
+                      <div className="col-6">
+                        <input
+                          type="text"
+                          name="address"
+                          className="formItem"
+                          minLength="3"
+                          maxLength="4"
+                          placeholder="5070"
+                          {...register("port", {
+                            ...requiredValidator,
+                            ...numberValidator,
+                            ...lengthValidator(3, 4),
+                          })}
+                          onKeyDown={restrictToNumbers}
+                        />
+                        {errors.port && <ErrorMessage text={errors.port.message} />}
+                      </div>
+                    </div>
+
+                    <div className="formRow col-xl-3">
+                      <div className="formLabel">
+                        <label className="text-dark">Serial Number</label>
+                        <label htmlFor="data" className="formItemDesc">
+                          Enter serial number
+                        </label>
+                      </div>
+                      <div className="col-6">
+                        <input
+                          type="text"
+                          name="serial_number"
+                          className="formItem"
+                          placeholder="124abc"
+                          {...register("serial_number", {
+                            ...requiredValidator,
+                          })}
+                          onKeyDown={restrictToMacAddress}
+                        />
+                        {errors.serial_number && (
+                          <ErrorMessage text={errors.serial_number.message} />
+                        )}
+                      </div>
+                    </div>
+                  </form>
                 </div>
               </div>
-
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Transport</label>
-                  <label htmlFor="data" className="formItemDesc">
-                    Select a transport
-                  </label>
-                </div>
-                <div className="col-6">
-                  <select
-                    value={watch().transport}
-                    className="formItem"
-                    name="transport "
-                    id="selectFormRow"
-                    {...register("transport", {
-                      ...requiredValidator,
-                    })}
-                  >
-                    <option disabled value="">
-                      Chose a vendor
-                    </option>
-                    <option selected value="TCPpreferred">
-                      TCP Preferred
-                    </option>
-                    <option value="UDPOnly">UDP Only</option>
-                    <option value="TLS">TLS</option>
-                    <option value="TCPOnly">TCP Only</option>
-                  </select>
-                  {errors.transport && (
-                    <ErrorMessage text={errors.transport.message} />
-                  )}
-                </div>
-              </div>
-
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Port</label>
-                  <label htmlFor="data" className="formItemDesc">
-                    Select a port
-                  </label>
-                </div>
-                <div className="col-6">
-                  <input
-                    type="text"
-                    name="address"
-                    className="formItem"
-                    minLength="3"
-                    maxLength="4"
-                    placeholder="5070"
-                    {...register("port", {
-                      ...requiredValidator,
-                      ...numberValidator,
-                      ...lengthValidator(3, 4),
-                    })}
-                    onKeyDown={restrictToNumbers}
-                  />
-                  {errors.port && <ErrorMessage text={errors.port.message} />}
-                </div>
-              </div>
-
-              <div className="formRow col-xl-3">
-                <div className="formLabel">
-                  <label className="text-dark">Serial Number</label>
-                  <label htmlFor="data" className="formItemDesc">
-                    Enter serial number
-                  </label>
-                </div>
-                <div className="col-6">
-                  <input
-                    type="text"
-                    name="serial_number"
-                    className="formItem"
-                    placeholder="124abc"
-                    {...register("serial_number", {
-                      ...requiredValidator,
-                    })}
-                    onKeyDown={restrictToMacAddress}
-                  />
-                  {errors.serial_number && (
-                    <ErrorMessage text={errors.serial_number.message} />
-                  )}
-                </div>
-              </div>
-            </form>
+            </div>
           </div>
+
         </div>
       </section>
       {popUp ? (
@@ -305,14 +370,17 @@ const DeviceProvisioningEdit = () => {
                     )}
 
                     <button
-                      className="panelButtonWhite m-0 float-end"
+                      className="panelButton gray m-0 float-end"
                       onClick={() => {
                         setPopUp(false);
                         setDeleteId("");
                         // setDeleteToggle(false);
                       }}
                     >
-                      Cancel
+                      <span className="text">Cancel</span>
+                      <span className="icon">
+                        <i class="fa-solid fa-xmark"></i>
+                      </span>
                     </button>
                   </div>
                 </div>
