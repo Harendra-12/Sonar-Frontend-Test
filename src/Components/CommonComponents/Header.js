726305b6a55ba042b08392c775cf3b7ae992ddc6
abhimanyu-webvio <abhimanyu.singh@webviotechnologies.com>
2024-12-24 15:41:38 +0530

    test

--- src/Components/CommonComponents/Header.js
+++ src/Components/CommonComponents/Header.js
@@ -3,8 +3,12 @@ import "tippy.js/dist/tippy.css";
 import React, { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { generalGetFunction } from "../GlobalFunction/globalFunction";
+import {
+  generalGetFunction,
+  generalPostFunction,
+} from "../GlobalFunction/globalFunction";
 import DarkModeToggle from "./DarkModeToggle";
+import { toast } from "react-toastify";
 
 function Header(props) {
   const dispatch = useDispatch();
@@ -13,7 +17,58 @@ function Header(props) {
   const balance = useSelector((state) => state.balance);
   const [accounName, setAccountName] = useState();
   const [dropDown, setDropDown] = useState(false);
+  const [popUp, setPopUp] = useState(false);
   const navigate = useNavigate();
+  const [oldPassword, setOldPassword] = useState("");
+  const [errorOldPassword, setErrorOldPassword] = useState(false);
+  const [newPassword, setNewPassword] = useState("");
+  const [errorNewPassword, setErrorNewPassword] = useState(false);
+  const [confPassword, setConfPassword] = useState("");
+  const [errorConfirm, setErrorConfirm] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const loadings = useSelector((state) => state.loading);
+
+  async function handleSubmit() {
+    if (oldPassword === "") {
+      setErrorOldPassword(true);
+    } else {
+      setErrorOldPassword(false);
+    }
+    if (newPassword.length < 4) {
+      setErrorNewPassword(true);
+    } else {
+      setErrorNewPassword(false);
+    }
+    if (confPassword !== newPassword) {
+      setErrorConfirm(true);
+    } else {
+      setErrorConfirm(false);
+    }
+
+    if (
+      oldPassword !== "" &&
+      newPassword !== "" &&
+      newPassword.length > 3 &&
+      confPassword === newPassword
+    ) {
+      setLoading(true);
+      const parsedData = {
+        old_password: oldPassword,
+        new_password: newPassword,
+      };
+      setPopUp(false);
+      const apiData = await generalPostFunction("/change-password", parsedData);
+      if (apiData?.status) {
+        setLoading(false);
+        setConfPassword("");
+        setOldPassword("");
+        setNewPassword("");
+        toast.success(apiData.message);
+      } else {
+        setLoading(false);
+      }
+    }
+  }
   useEffect(() => {
     if (account === null || account === "") {
       navigate("/");
@@ -125,24 +180,31 @@ function Header(props) {
               <Link to={"/change-password"}>Change Password</Link>
             </div> */}
             <div className="dropdown-item">
-              <Link
+              {/* <Link
                 to={"/change-password"}
                 className="clearButton text-align-start"
               >
                 <i className="fa-regular fa-lock me-2"></i>
                 Change Password
-              </Link>
+              </Link> */}
+              <div
+                className="clearButton text-align-start"
+                onClick={() => setPopUp(true)}
+              >
+                <i className="fa-regular fa-lock me-2"></i>
+                Change Password
+              </div>
             </div>
             <div className="dropdown-item">
-              <Link
-                to={"/my-profile"}
-                className="clearButton"
-              >
+              <Link to={"/my-profile"} className="clearButton">
                 <i class="fa-regular fa-user me-2" aria-hidden="true"></i>
                 Profile
               </Link>
             </div>
-            <div onClick={logOut} className="d-flex w-100 gap-3 align-items-center justify-content-start">
+            <div
+              onClick={logOut}
+              className="d-flex w-100 gap-3 align-items-center justify-content-start"
+            >
               <Link to={"/"} className="logoutBtn">
                 <i className="fa-solid fa-power-off me-1"></i> Logout
               </Link>
@@ -152,6 +214,172 @@ function Header(props) {
           ""
         )}
       </div>
+      {popUp ? (
+        <div className="popup">
+          <div className="container h-100">
+            <div className="row h-100 justify-content-center align-items-center">
+              <div className="row content col-xl-4">
+                <div className="col-2 px-0">
+                  <div className="iconWrapper">
+                    <i
+                      className="fa fa-key text-primary"
+                      aria-hidden="true"
+                    ></i>
+                  </div>
+                </div>
+                <div className="col-10 ps-0">
+                  <h4>Change Password!</h4>
+                  <div className="col-12" style={{ padding: "25px 23px" }}>
+                    <form>
+                      <div className="formRow col-12">
+                        <div className="formLabel">
+                          <div>
+                            <label className="text-dark">
+                              Current Password :{" "}
+                            </label>
+                          </div>
+                          <div>
+                            <label
+                              htmlFor="data"
+                              className="formItemDesc"
+                              style={{
+                                fontSize: 12,
+                                lineHeight: "18px",
+                                marginTop: 5,
+                              }}
+                            >
+                              This is your current password.
+                            </label>
+                          </div>
+                        </div>
+                        <div className="col-12">
+                          <input
+                            type="text"
+                            name="extension"
+                            className="formItem"
+                            value={oldPassword}
+                            onChange={(e) => setOldPassword(e.target.value)}
+                          />
+                          {errorOldPassword ? (
+                            <label className="status missing">
+                              Field Missing
+                            </label>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      </div>
+                      <div className="formRow col-12">
+                        <div className="formLabel">
+                          <div>
+                            <label className="text-dark">New Password : </label>
+                          </div>
+                          <div>
+                            <label
+                              htmlFor="data"
+                              className="formItemDesc"
+                              style={{
+                                fontSize: 12,
+                                lineHeight: "18px",
+                                marginTop: 5,
+                              }}
+                            >
+                              Please input your preferred new password.
+                            </label>
+                          </div>
+                        </div>
+                        <div className="col-12">
+                          <input
+                            type="text"
+                            name="extension"
+                            className="formItem"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                          />
+                          {errorNewPassword ? (
+                            <label className="status missing">
+                              Password must be at least 6 characters
+                            </label>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      </div>
+                      <div className="formRow col-12">
+                        <div className="formLabel">
+                          <div>
+                            <label className="text-dark">
+                              Confirm Password :
+                            </label>
+                            {errorConfirm ? (
+                              <label className="status missing">
+                                Password do not matched
+                              </label>
+                            ) : (
+                              ""
+                            )}
+                          </div>
+                          <div>
+                            <label
+                              htmlFor="data"
+                              className="formItemDesc"
+                              style={{
+                                fontSize: 12,
+                                lineHeight: "18px",
+                                marginTop: 5,
+                              }}
+                            >
+                              Please retype your password.
+                            </label>
+                          </div>
+                        </div>
+                        <div className="col-12">
+                          <input
+                            type="text"
+                            name="extension"
+                            className="formItem"
+                            value={confPassword}
+                            onChange={(e) => setConfPassword(e.target.value)}
+                          />
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                  <div className="mt-2 d-flex justify-content-between">
+                    <button
+                      disabled={loading}
+                      className="panelButton m-0"
+                      onClick={() => handleSubmit()}
+                    >
+                      <span className="text">Confirm</span>
+                      <span className="icon">
+                        <i class="fa-solid fa-check"></i>
+                      </span>
+                    </button>
+                    {/* ) : ( */}
+
+                    {/* )} */}
+
+                    <button
+                      className="panelButton gray m-0 float-end"
+                      onClick={() => {
+                        setPopUp(false);
+                      }}
+                    >
+                      <span className="text">Cancel</span>
+                      <span className="icon">
+                        <i class="fa-solid fa-xmark"></i>
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
