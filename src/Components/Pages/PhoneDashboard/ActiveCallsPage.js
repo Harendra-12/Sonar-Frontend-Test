0f6bc77b5b4a09f94c37797a6d1e998e7dd81739
damini-webvio <damini.raj@webviotechnologies.com>
2025-02-28 19:04:32 +0530

    call Dashboard issue fix dynamic sidebar fix

--- src/Components/Pages/PhoneDashboard/ActiveCallsPage.js
+++ src/Components/Pages/PhoneDashboard/ActiveCallsPage.js
@@ -364,15 +364,15 @@ function ActiveCallsPage() {
                                                     <div className={`deviceProvision `} >
                                                         <div className="itemWrapper a">
                                                             <div className="heading h-auto d-block">
-                                                                <h5>{item.model_type === "CallCenterQueue" ? item.model.queue_name : item.model_type === "Ringgroup" ? item.model.name : item.model.did}</h5>
-                                                                <p>{item.model_type}</p>
+                                                                <h5>{item?.model_type === "CallCenterQueue" ? item?.model?.queue_name : item?.model_type === "Ringgroup" ? item?.model?.name : item?.model?.did}</h5>
+                                                                <p>{item?.model_type}</p>
                                                             </div>
                                                             <div className="data-number2 h-auto">
                                                                 <div className="d-flex flex-wrap justify-content-between">
                                                                     <div className="col-4">
                                                                         <p>Active</p>
                                                                         <h4>
-                                                                            {filterActiveState(item.model_type, item.model_type === "CallCenterQueue" ? item.model.extension : item.model_type === "Ringgroup" ? item.model.extension : item.model.did)}{" "}
+                                                                            {filterActiveState(item?.model_type, item?.model_type === "CallCenterQueue" ? item?.model?.extension : item?.model_type === "Ringgroup" ? item?.model?.extension : item?.model?.did)}{" "}
                                                                             <i
                                                                                 className="fa-solid fa-phone-volume ms-1"
                                                                                 style={{ color: "var(--funky-boy4)", fontSize: 17 }}
@@ -382,7 +382,7 @@ function ActiveCallsPage() {
                                                                     <div className="col-4 text-center">
                                                                         <p>Ringing</p>
                                                                         <h4>
-                                                                            {filterRingingState(item.model_type, item.model_type === "CallCenterQueue" ? item.model.extension : item.model_type === "Ringgroup" ? item.model.extension : item.model.did)}{" "}
+                                                                            {filterRingingState(item?.model_type, item?.model_type === "CallCenterQueue" ? item?.model?.extension : item?.model_type === "Ringgroup" ? item?.model?.extension : item?.model?.did)}{" "}
                                                                             <i
                                                                                 className="fa-solid fa-phone-office ms-1"
                                                                                 style={{ color: "rgb(1, 199, 142)", fontSize: 17 }}
--- src/Components/Pages/PhoneDashboard/RingGroupDetails.js
+++ src/Components/Pages/PhoneDashboard/RingGroupDetails.js
@@ -102,7 +102,7 @@ const RingGroup = () => {
                       <td>{call.name}</td>
                       <td>
                         {
-                          activeCallData.filter((e) => e.dest === call.extension)
+                          activeCallData.filter((e) => e.dest === call.extension&&( e.b_callstate === "ACTIVE" || e.b_callstate === "HELD"))
                             .length
                         }
                       </td>
