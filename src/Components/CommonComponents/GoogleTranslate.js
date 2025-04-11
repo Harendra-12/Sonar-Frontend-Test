0daeb5e36bb5b876ca7ebffe7be15f0f81919602
mauryarishabh9 <rishabh.maurya@webviotechnologies.com>
2025-04-11 19:01:59 +0530

    select
    ui fix

--- src/Components/CommonComponents/GoogleTranslate.js
+++ src/Components/CommonComponents/GoogleTranslate.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 
 const GoogleTranslate = () => {
+<<<<<<< Updated upstream
     useEffect(() => {
         const addGoogleTranslateScript = () => {
             const script = document.createElement("script");
@@ -61,6 +62,103 @@ const GoogleTranslate = () => {
 
     return <div id="google_translate_element"></div>;
     
+=======
+  useEffect(() => {
+    // Define the callback globally
+    window.googleTranslateElementInit = () => {
+      new window.google.translate.TranslateElement(
+        { pageLanguage: "en" },
+        "google_translate_element"
+      );
+
+      const listenForLanguageChange = () => {
+        const iframe = document.querySelector("iframe.goog-te-menu-frame");
+        if (iframe) {
+          const iframeDocument =
+            iframe.contentDocument || iframe.contentWindow.document;
+
+          const observer = new MutationObserver(() => {
+            const languageSelect = iframeDocument.querySelector(
+              ".goog-te-menu2-item-selected"
+            );
+            if (languageSelect) {
+              const selectedLang = languageSelect.textContent.trim();
+              localStorage.setItem("selected_language", selectedLang);
+            }
+          });
+
+          observer.observe(iframeDocument.body, {
+            childList: true,
+            subtree: true,
+          });
+        }
+      };
+
+      setTimeout(listenForLanguageChange, 1000);
+    };
+
+    const addGoogleTranslateScript = () => {
+      if (!document.querySelector("script[src*='translate_a/element.js']")) {
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src =
+          "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+        script.async = true;
+        document.body.appendChild(script);
+      }
+    };
+
+    addGoogleTranslateScript();
+
+    // Optional cleanup
+    return () => {
+      const script = document.querySelector(
+        "script[src*='translate_a/element.js']"
+      );
+      if (script) script.remove();
+    };
+  }, []);
+
+  return (
+    // <Rnd
+    //     minWidth={"300px"}
+    //     minHeight={"450px"}
+    //     maxWidth={"300px"}
+    //     maxHeight={"450px"}
+    //     dragHandleClassName="drag-handle" // Specify draggable area
+    // >
+    //     <div
+    //         style={{
+    //             background: "transparent",
+    //             position: "relative",
+    //             zIndex: "999",
+    //         }}
+    //     >
+    //         {/* Draggable Top Area */}
+    //         <div
+    //             id="google_translate_element"
+    //             className="drag-handle"
+    //             style={{
+    //                 position: "absolute",
+    //                 top: "35px",
+    //                 width: "100%",
+    //                 height: "105px",
+    //                 zIndex: "999999",
+    //                 background: "transparent",
+    //                 cursor: "move",
+    //             }}
+    //         ></div>
+    //     </div>
+    // </Rnd>
+
+    <div className="sticky-icon">
+      <div id="google_translate_element"></div>
+      <div class="">
+      <i class="fa-solid fa-gears"></i>
+      </div>
+    </div>
+  );
+>>>>>>> Stashed changes
 };
 
 {/* <div class="sticky-icon">
