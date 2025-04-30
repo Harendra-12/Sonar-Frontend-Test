1d2e0d18e3121ad76faa6a8119296b0a33851e4b
webviosystem10 <riddhee25@gmail.com>
2025-04-30 13:49:29 +0530

    Integrated necessary APIs into MessageProfileDetails

--- src/Components/Pages/WebRtc/components/MessageProfileDetails.js
+++ src/Components/Pages/WebRtc/components/MessageProfileDetails.js
@@ -1,20 +1,86 @@
 import React, { useEffect, useState } from "react";
+import { formatTimeWithAMPM, generalGetFunction } from "../../../GlobalFunction/globalFunction";
+import { Link, useNavigate } from "react-router-dom";
+import EmptyPrompt from "../../../Loader/EmptyPrompt";
+import { toast } from "react-toastify";
 
 const MessageProfileDetails = ({ recipient, messages }) => {
+  const navigate = useNavigate();
   // console.log("Messages",messages);
-  const [media, setMedia] = useState([])
-  const [files, setFiles] = useState([])
+  const [media, setMedia] = useState([]);
+  const [allMedia, setAllMedia] = useState([]);
 
-  console.log('+++++++++++++++++++', messages);
+  const [viewAllToggle, setViewAllToggle] = useState(false);
 
+  const [files, setFiles] = useState([]);
+  const [allFiles, setAllFiles] = useState([]);
 
+  // Set Media and Files
   useEffect(() => {
-    setMedia(messages?.filter((item) => item.message_type === "image" || item.message_type === "video" || item.message_type === "audio"))
-    setFiles(messages?.filter((item) => item.message_type === "file"))
-  }, [messages])
+    if (viewAllToggle === "files") {
+      setFiles(allFiles);
+    } else if (viewAllToggle === "media") {
+      setMedia(allMedia);
+    } else {
+      setMedia(messages?.filter((item) => item.message_type === "image" || item.message_type === "video" || item.message_type === "audio").slice(0, 4))
+      setFiles(messages?.filter((item) => item.message_type === "file").slice(0, 4));
+    }
+  }, [messages, viewAllToggle])
 
-  console.log("Media", media);
-  console.log("File", files)
+  // Download any File / Media Function
+  const downloadImage = async (imageUrl, fileName) => {
+    try {
+      const response = await fetch(imageUrl);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", fileName);
+
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading the image:", error);
+    }
+  };
+
+  // Handle View All Files and Media
+  const handleViewAll = async (type, chat) => {
+    if (type === "files") {
+      try {
+        const response = await generalGetFunction(`message/all-filters?receiver_id=${recipient[1]}&message_type=files`);
+        if (response.status) {
+          setAllFiles(response.data);
+          setViewAllToggle("files");
+          toast.success(response.message)
+        } else {
+          toast.error(response.message)
+        }
+      } catch (err) {
+        console.error("Error fetching files:", err);
+      }
+    } else if (type === "media") {
+      try {
+        const response = await generalGetFunction(`message/all-filters?receiver_id=${recipient[1]}&message_type=media`);
+        if (response.status) {
+          setAllMedia(response.data);
+          setViewAllToggle("media");
+          toast.success(response.message)
+        } else {
+          toast.error(response.message)
+        }
+      } catch (err) {
+        console.error("Error fetching files:", err);
+      }
+    }
+  }
 
   return (
     <div className="messageOverlay py-3 h-100">
@@ -59,107 +125,82 @@ const MessageProfileDetails = ({ recipient, messages }) => {
         </div>
       </div>
       <div className="rightPanel">
-        <div className="chat_doc px-4 mb-4">
-          <div className="d-flex justify-content-between align-items-center gap-2">
+        <div className="chat_doc px-4 mb-2">
+          <div className="d-flex justify-content-between gap-2">
             <p>
               Shared Files
               <span className="badge badge-purple fw-medium rounded-circle ms-2">
-                4
-              </span>
-              <span>
-                {/* <Link className="" to=""><u>View All</u>
-            </Link> */}
+                {files?.length || 0}
               </span>
             </p>
-          </div>
-          <div className="file_list">
-            <div className="">
-              <span className="shared-file-icon">
-                <i className="fa-regular fa-files"></i>
+            {files && files.length > 0 ? (
+              <span>
+                <a onClick={() => viewAllToggle ? setViewAllToggle(false) : handleViewAll('files', recipient[2])}><u>{viewAllToggle === "files" ? 'View Less' : 'View All'}</u></a>
               </span>
-            </div>
-            <div className=" ">
-              <p className="ellipsisText">Project Details.pdf</p>
-              <p className="text_muted">14,April 2025 - 14:24PM</p>
-            </div>
-            <div className="download ">
-              <button>
-                <i className="fa-regular fa-arrow-down-to-line"></i>
-              </button>
-            </div>
+            ) : ""}
           </div>
-          <div className="file_list">
-            <div className="">
-              <span className="shared-file-icon">
-                <i class="fa-regular fa-images"></i>
-              </span>
-            </div>
-            <div className=" ">
-              <p className="ellipsisText">Img_02.Jpg</p>
-              <p className="text_muted">22,April 2025 - 10:19AM</p>
-            </div>
-            <div className="download ">
-              <button>
-                <i className="fa-regular fa-arrow-down-to-line"></i>
-              </button>
-            </div>
-          </div>
-          <div className="file_list">
-            <div className="">
-              <span className="shared-file-icon">
-                <i class="fa-sharp fa-regular fa-film"></i>
-              </span>
-            </div>
-            <div className=" ">
-              <p className="ellipsisText">Video_15_02_2022.MP4</p>
-              <p className="text_muted">22,April 2025 - 10:19AM</p>
-            </div>
-            <div className="download ">
-              <button>
-                <i className="fa-regular fa-arrow-down-to-line"></i>
-              </button>
-            </div>
-          </div>
+          {
+            viewAllToggle === "files" || !viewAllToggle ? (
+              files && files?.length > 0 ? (
+                files.map((item, index) => (
+                  <div className="file_list" key={index}>
+                    <div className="">
+                      <span className="shared-file-icon">
+                        <i className="fa-regular fa-files"></i>
+                      </span>
+                    </div>
+                    <div className=" ">
+                      <p className="ellipsisText">{item.body.split('chats/')[1]}</p>
+                      <p className="text_muted">{item.time.split(" ")[0]} - {formatTimeWithAMPM(item.time.split(" ")[1])}</p>
+                    </div>
+                    <div className="download" onClick={() => downloadImage(item.body, item.body.split('chats/')[1])} >
+                      <button>
+                        <i className="fa-regular fa-arrow-down-to-line"></i>
+                      </button>
+                    </div>
+                  </div>
+                ))
+              ) : <EmptyPrompt generic={true} small={true} nomargin={true} />
+            ) : ""}
         </div>
         <div className="chat_doc px-4">
-          <div className="d-flex justify-content-between align-items-center gap-2">
+          <div className="d-flex justify-content-between gap-2">
             <p>
               Photos & Media
               <span className="badge badge-purple fw-medium rounded-circle ms-2">
-                4
-              </span>
-              <span>
-                {/* <Link className="" to=""><u>View All</u>
-            </Link> */}
+                {media?.length || 0}
               </span>
             </p>
+            {media && media.length > 0 ? (
+              <span>
+                <a onClick={() => viewAllToggle ? setViewAllToggle(false) : handleViewAll('media', recipient[2])}><u>{viewAllToggle === "media" ? 'View Less' : 'View All'}</u></a>
+              </span>
+            ) : ""}
           </div>
-          <div className="imageList">
-            <div className="imgBox">
-              <img
-                src={require("../../../assets/images/profilepic.png")}
-                alt=""
-              />
-            </div>
-            <div className="imgBox">
-              <img
-                src={require("../../../assets/images/profilepic.png")}
-                alt=""
-              />
-            </div>
-            <div className="imgBox">
-              <img
-                src={require("../../../assets/images/profilepic.png")}
-                alt=""
-              />
-            </div>
-            <div className="imgBox">
-              <img
-                src={require("../../../assets/images/profilepic.png")}
-                alt=""
-              />
-            </div>
-          </div>
+
+          {viewAllToggle === "media" || !viewAllToggle ? (
+            media && media?.length > 0 ? (
+              <div className="imageList">
+                {media.map((item, index) => (
+                  <div className="imgBox" key={index}>
+                    <img
+                      src={item.body}
+                      onError={(e) => e.target.src = require('../../../assets/images/placeholder-image2.webp')}
+                      alt=""
+                    />
+                    <div className="extraButtons">
+                      <Link className="tableButton me-2" to={item.body} target="_blank">
+                        <i className="fa-solid fa-eye text-white" />
+                      </Link>
+                      <button className="tableButton head ms-2" onClick={() => downloadImage(item.body, item.body.split('chats/')[1])}>
+                        <i className="fa-solid fa-download" />
+                      </button>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            ) : <EmptyPrompt generic={true} small={true} nomargin={true} />
+          ) : ""}
         </div>
       </div>
 
