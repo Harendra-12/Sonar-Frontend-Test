b0834f44757cfb94f3fd59d09d974acf7684d547
webviosystem10 <riddhee25@gmail.com>
2025-05-16 13:57:59 +0530

    Update IncomingCallPopup.js

--- src/Components/Pages/WebRtc/IncomingCallPopup.js
+++ src/Components/Pages/WebRtc/IncomingCallPopup.js
@@ -85,7 +85,7 @@ function IncomingCallPopup({
     }
 
     // If a call is already established, then set incoming call to mute
-    if (globalSession?.length > 1 && globalSession[0]?.state == "Established") {
+    if (globalSession?.length > 1) {
       setMuteAudio(true);
     }
   }, [sessionId, globalSession]);
