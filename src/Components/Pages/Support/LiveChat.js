8bde06c98d6832fc44eee9f3dc49ce8a1cfc5889
webviosystem10 <webviosystem10@gmail.com>
2025-05-31 14:22:50 +0530

    Update LiveChat.js

--- src/Components/Pages/Support/LiveChat.js
+++ src/Components/Pages/Support/LiveChat.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Header from '../../CommonComponents/Header';
 import { Navigate, useLocation } from 'react-router-dom';
 import { backToTop, featureUnderdevelopment } from '../../GlobalFunction/globalFunction';
