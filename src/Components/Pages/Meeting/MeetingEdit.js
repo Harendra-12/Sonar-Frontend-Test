a73ed76d296b0890bd9149237806b6357389dc2e
webviosystem10 <webviosystem10@gmail.com>
2025-06-23 16:53:45 +0530

    Bug fixes in Meeting

--- src/Components/Pages/Meeting/MeetingEdit.js
+++ src/Components/Pages/Meeting/MeetingEdit.js
@@ -49,7 +49,7 @@ function MeetingEdit() {
       const response = await generalGetFunction(`/conference/${id}`);
       if (response.status) {
         const data = response.data;
-        if (data.conf_start_time && data.conf_end_time) {
+        if (data.conf_start_time || data.conf_end_time) {
           setValue("conf_scheduled", "1")
         }
         if (Array.isArray(data.users) && data.users.length === 0) {
@@ -69,10 +69,10 @@ function MeetingEdit() {
   }
 
   const handleMeetingForm = handleSubmit(async (data) => {
-    // if (participants.length == 1 && participants[0].length == 0) {
-    //   toast.error("Please add participants");
-    //   return;
-    // }
+    if (participants.length == 1 && participants[0].length == 0) {
+      toast.error("Please add participants");
+      return;
+    }
     if (
       watch().conf_type !== "internal" &&
       (members === null || members === "")
@@ -80,8 +80,15 @@ function MeetingEdit() {
       toast.error("Please enter number of members");
     } else {
       setLoading(true);
+
+      const initialData = data;
+      if (watch().conf_scheduled !== "1") {
+        delete initialData.conf_start_time;
+        delete initialData.conf_end_time;
+      }
+
       const parsedData = {
-        ...data,
+        ...initialData,
         ...(watch().conf_type == "internal" && addedUsers.length > 0
           ? { users: addedUsers.map((user) => user.id) }
           : {}),
