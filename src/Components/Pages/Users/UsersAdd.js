8a02c942e8622ff531985e960a0f908c78897d37
webviosystem8 - Pratima <pratima.majumder@webviotechnologies.com>
2025-07-01 18:56:14 +0530

    changes

--- src/Components/Pages/Users/UsersAdd.js
+++ src/Components/Pages/Users/UsersAdd.js
@@ -25,6 +25,9 @@ import Header from "../../CommonComponents/Header";
 import { toast } from "react-toastify";
 import { PermissionConfigTable } from "../../CommonComponents/PermissionConfigForUser";
 const UsersAdd = () => {
+  const pratima = null;
+  console.log(pratima.length);
+  
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const allUserRefresh = useSelector((state) => state.allUserRefresh);
--- src/ErrorBoundaryWithoutNavigate.js
+++ src/ErrorBoundaryWithoutNavigate.js
@@ -24,17 +24,23 @@ class ErrorBoundaryWithoutHooks extends React.Component {
   }
 
   handleGoBack = () => {
-    this.props.navigate(-1);
+    this.props.navigate("/dashboard");
   };
 
   render() {
     if (this.state.hasError) {
       return (
-        <div style={{ padding: 40, textAlign: "center" }}>
-          <h2>Something went wrong.</h2>
-          {/* <p>{this.state.error?.message}</p> */}
-          <button onClick={this.handleGoBack}>Go Back</button>
-        </div>
+        <main className="mainContent">
+          <div className="d-flex flex-column align-items-center justify-content-center " style={{ height: "100vh" }}>
+            <div className="wrongImg">
+              <img src={require("./Components/assets/images/went-wrong.png")} alt="background" />
+            </div>
+            <h2>Something went wrong.</h2>
+            <p>Brack yourself till we get the error fixed.</p>
+            {/* <p>{this.state.error?.message}</p> */}
+            <button onClick={this.handleGoBack} class="panelButton static add mt-3" type="submit"><span class="text text-white">Home</span></button>
+          </div>
+        </main>
       );
     }
 
