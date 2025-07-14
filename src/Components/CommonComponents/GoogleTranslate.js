b4801a77a7f0bccb18aef41808c187a0c4329a58
RavirajO7 <raviraj0884@gmail.com>
2025-07-14 17:24:07 +0530

    get did done

--- src/Components/CommonComponents/GoogleTranslate.js
+++ src/Components/CommonComponents/GoogleTranslate.js
@@ -2,17 +2,23 @@ import { useEffect } from "react";
 
 const GoogleTranslate = () => {
   useEffect(() => {
-    // Define Google's init function globally
+    // Define the init function globally
     window.googleTranslateElementInit = () => {
-      new window.google.translate.TranslateElement(
-        { pageLanguage: "en" },
-        "google_translate_element"
-      );
+      if (!document.getElementById("google_translate_element").innerHTML.trim()) {
+        new window.google.translate.TranslateElement(
+          { pageLanguage: "en" },
+          "google_translate_element"
+        );
+      }
     };
 
-    // Load Google Translate script
     const addGoogleTranslateScript = () => {
-      if (!document.querySelector("script[src*='translate_a/element.js']")) {
+      const existingScript = document.querySelector("script[src*='translate_a/element.js']");
+      
+      if (existingScript) {
+        // If script is already there, just re-trigger the callback
+        window.googleTranslateElementInit();
+      } else {
         const script = document.createElement("script");
         script.src =
           "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
@@ -21,10 +27,15 @@ const GoogleTranslate = () => {
       }
     };
 
+    // Cleanup previously inserted Translate element on re-renders (optional)
+    const translateDiv = document.getElementById("google_translate_element");
+    if (translateDiv) {
+      translateDiv.innerHTML = "";
+    }
+
     addGoogleTranslateScript();
   }, []);
 
-  // Function to programmatically open the language dropdown
   const openLanguageDropdown = () => {
     const googleSpan = document.querySelector(".goog-te-gadget span");
     if (googleSpan) {
@@ -33,21 +44,20 @@ const GoogleTranslate = () => {
   };
 
   return (
-    <div className="language-selector me-3" style={{ position: "relative", zIndex: 9999 }} >
+    <div className="language-selector me-3" style={{ position: "relative", zIndex: 9999 }}>
       <i className="fa-solid fa-language language-icon"></i>
 
       <i
-        className="fa-solid fa-chevron-down dropdown-icon" onClick={openLanguageDropdown}
-
+        className="fa-solid fa-chevron-down dropdown-icon"
+        onClick={openLanguageDropdown}
         style={{ cursor: "pointer", marginLeft: "5px" }}
         title="Change Language"
       ></i>
-      <div id="google_translate_element" style={{ position: "absolute", top: "0px", opacity: 0, left: "0" }}></div>
-      {/* <span className="language-text" style={{ marginLeft: "5px" }}>
-        EN
-      </span> */}
 
-
+      <div
+        id="google_translate_element"
+        style={{ position: "absolute", top: "0px", opacity: 0, left: "0" }}
+      ></div>
     </div>
   );
 };
