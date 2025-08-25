69c933840dfd70cf337bb1a8c4dce05657d28ad3
RavirajO7 <raviraj0884@gmail.com>
2025-08-25 13:07:32 +0530

    Revert "email verification added on listing"
    This reverts commit 1d78cb709d4bcc4625e02ab3308c759275cc6ad2.

--- src/Components/Pages/Users/Users.js
+++ src/Components/Pages/Users/Users.js
@@ -515,7 +515,6 @@ const Users = () => {
                             <th>Extension</th>
                             <th>Role</th>
                             <th>Usage</th> */}
-                           
                               {tableKeys.includes("socket_status") && (
                                 <th className="text-center">
                                   {" "}
@@ -538,7 +537,6 @@ const Users = () => {
                                   </select>
                                 </th>
                               )}
-                               <th className="text-center">Verification</th>
                               {checkViewSidebar(
                                 "User",
                                 slugPermissions,
@@ -735,13 +733,6 @@ const Users = () => {
                                                   : "extensionStatus mx-auto"
                                               }
                                             ></span>
-                                          </td>
-                                        )}
-                                         {tableKeys.includes(
-                                          "email_verified_at"
-                                        ) && (
-                                          <td style={{ width: "156px" }}>
-                                            {item.email_verified_at?"Verified":"Not Verify"}
                                           </td>
                                         )}
                                         {checkViewSidebar(
